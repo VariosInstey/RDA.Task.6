adf10ff1dc946222f354a547987076544629f858
VariosInstey <124655869+VariosInstey@users.noreply.github.com>
2023-03-20 23:14:03 +0300

    Добавьте файлы проекта.

---
+++ RDA.Task.6/View/Pages/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RDA.Task._6.View.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        private bool _stateClosed = true;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void ButtonMenu_Click(object sender, RoutedEventArgs e)
+        {
+            if (_stateClosed)
+            {
+                Storyboard? sb = FindResource("OpenMenu") as Storyboard;
+                sb?.Begin();
+            }
+            else
+            {
+                Storyboard? sb = FindResource("CloseMenu") as Storyboard;
+                sb?.Begin();
+            }
+            _stateClosed = !_stateClosed;
+        }
+
+        private void ButtonMenu_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (_stateClosed)
+            {
+                Storyboard? sb = FindResource("OpenMenu") as Storyboard;
+                sb?.Begin();
+            }
+            else
+            {
+                Storyboard? sb = FindResource("CloseMenu") as Storyboard;
+                sb?.Begin();
+            }
+            _stateClosed = !_stateClosed;
+        }
+    }
+}
